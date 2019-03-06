5c78a452145c8a5103ae94c43486c18bc824ce40
fdf_100000581038_fengchuanyu <fdf_100000581038_fengchuanyu@git.cloud.tencent.com>
2019-03-06 11:08:14 +0800

    admin

--- src/main.js
+++ src/main.js
@@ -3,10 +3,14 @@ import './plugins/axios'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
 import defaultSetting from './utils/defaultSettings'
 
 Vue.config.productionTip = false
 Vue.prototype.defaultSetting = defaultSetting;
+Vue.use(ElementUI);
+
 new Vue({
   router,
   store,
